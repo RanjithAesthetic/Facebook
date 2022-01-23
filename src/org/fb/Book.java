295841d77e32b6f633e0371160cd28d4937bc6cc
Ranjithasthetic <ranjithraghul1@gmail.com>
2022-01-23 23:37:21 +0530

    vin

--- src/org/fb/Book.java
+++ src/org/fb/Book.java
@@ -4,5 +4,11 @@ package org.fb;
 */
 
 public class Book {
-
+	public void m2() {
+		System.out.println("hello");
+	}
+	public static void main(String[] args) {
+		Book b = new Book();
+		b.m2();
+	}
 }
